96fb88432f09e7e694637ca3dca4b5b774101d92
Vignesh Narendran <vignesh-narendran@users.noreply.github.com>
2020-03-01 13:02:56 +0530

    Updates

---
+++ precache-manifest.6c5e1e412de590b1b6ca5ae7156ea267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0660b6021e82e0e75f717f61b47318",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "4aa4e04e200778d7753e",
+    "url": "/weather-react-app/static/css/main.40c7721a.chunk.css"
+  },
+  {
+    "revision": "a7d866fa264e49dda176",
+    "url": "/weather-react-app/static/js/2.f1d618f7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weather-react-app/static/js/2.f1d618f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa4e04e200778d7753e",
+    "url": "/weather-react-app/static/js/main.f59b1a60.chunk.js"
+  },
+  {
+    "revision": "13b21d73f2997ce4839c",
+    "url": "/weather-react-app/static/js/runtime-main.7f3349e3.js"
+  }
+]);
